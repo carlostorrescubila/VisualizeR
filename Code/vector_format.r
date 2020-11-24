1862b5e5d850e7e5d383a12504a5f9f6abffd310
carlostorrescubila <44613865+carlostorrescubila@users.noreply.github.com>
2020-11-24 03:05:08 +0100

    vector_format function documented

--- Code/vector_format.r
+++ Code/vector_format.r
@@ -1,11 +1,18 @@
-#' Title
+#' @title Vector format
+#' 
+#' @description Convert the output of a vector into a character that contains a vector to use as input in R. 
 #'
-#' @param vector 
+#' @param vector character; \cr
+#' The output that user want to give the vector format.
 #'
-#' @return
-#' @export
-#'
+#' @return It returns a character that contains a vector as definied in R language. 
+#' 
 #' @examples
+#' iris %>% colnames %>% vector_format
+#' 
+#' @import glue
+#' 
+#' @export
 
 vector_format <- function(vector){
   
