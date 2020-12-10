32fbc9e21eee90219a518520c80335236a772347
carlostorrescubila <44613865+carlostorrescubila@users.noreply.github.com>
2020-12-10 09:29:18 +0100

    Create hexSticker logo

---
+++ Code/logo_maker.r
@@ -0,0 +1,21 @@
+# library(hexSticker)
+# library(showtext)
+# library(magrittr)
+# 
+# # icon in 512px:
+# # https://www.flaticon.com/free-icon/monitor_3219705
+# 
+# font_add_google("Lobster", "lobster")
+# 
+# sticker(
+#   subplot = "https://www.flaticon.com/svg/static/icons/svg/3219/3219705.svg",
+#   s_width = 0.5,
+#   s_x = 1, s_y = 0.75,
+#   package = "VisualizeR",
+#   p_family = "lobster",
+#   p_size = 25, p_color = "#006DF0",
+#   h_fill = "#e3e3e3",
+#   h_size = 1, h_color = "#006DF0",
+#   # filename = "www/VisualizeR.png"
+#   ) %>%
+#   print()
